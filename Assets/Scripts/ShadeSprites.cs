c639009c4c3dcf62a613ea75d8042f22ef3750e3
Yangying <ryysophie@126.com>
2021-02-01 02:22:12 +0800

    text

---
+++ Assets/Scripts/ShadeSprites.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShadeSprites : MonoBehaviour
+
+{
+private static float _start=0f;
+    public void Start(){
+        _start=0f;
+    }
+    public void Update(){
+
+        
+    
+        Color color=gameObject.GetComponent<SpriteRenderer>().color;
+        _start+=0.01f;
+        Color end=new Color(color.r,color.g,color.b,_start);
+
+        gameObject.GetComponent<SpriteRenderer>().color=Color.Lerp(color,end,1);
+        
+        
+
+    }
+}
