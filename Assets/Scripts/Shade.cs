1cb48dddeec10d1df8083aab6867e20a757e9c75
Yangying <ryysophie@126.com>
2021-01-31 14:59:01 +0800

    FadeScript

---
+++ Assets/Scripts/Shade.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Shade : MonoBehaviour
+
+{
+private static float _start=0f;
+    public void Update(){
+
+        
+        TextMeshPro textmeshPro = GetComponent<TextMeshPro>();
+        Color color=textmeshPro.color;
+        _start+=0.01f;
+        Color end=new Color(color.r,color.g,color.b,_start);
+
+        gameObject.GetComponent<TextMeshPro>().color=Color.Lerp(color,end,1);}
+        
+        
+
+    
+}
